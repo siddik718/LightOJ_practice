6c21456275b1865f7a66e1df03c32c9b185a9e22
Abu Bakar Siddik <70488808+siddik718@users.noreply.github.com>
2021-05-10 21:08:28 +0600

    Update Redirect URL.cpp

--- Redirect URL.cpp
+++ Redirect URL.cpp
@@ -1,19 +1,23 @@
 #include<bits/stdc++.h>
+
 using namespace std;
-int main(){
-int t,cnt = 1;
-string s;
-cin >> t;
-getchar();
-while(t--){
-    getline(cin,s);
-    cout << "Case " << cnt++ << ": ";
-    if(s[4] == 's')
-    cout << s << endl;
-    else{
+
+int main()
+{
+    int t,cnt = 1;
+    string s;
+    
+    cin >> t;
+    getchar();
+    
+    while(t--)
+    {
+        getline(cin,s);
+        if(s[4] != 's')
+        {
             s.insert(4,"s");
-        cout << s << endl;
+        }
+        cout <<  "Case " << cnt++ << ": " <<  s << endl;
     }
-}
-return 0;
+    return 0;
 }
